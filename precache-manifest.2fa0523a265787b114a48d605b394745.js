b968fc0e197f891db2fa3d2acb92d05de0247e56
boukamass <boukamass@gmail.com>
2020-07-31 18:44:00 -0500

    Updates

---
+++ precache-manifest.2fa0523a265787b114a48d605b394745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9923ca4b0c28d76bc2d804c77df1cdf4",
+    "url": "/react_news_app/index.html"
+  },
+  {
+    "revision": "4b1751b49ddd49430560",
+    "url": "/react_news_app/static/css/main.45a388bb.chunk.css"
+  },
+  {
+    "revision": "da1396417b421654aff2",
+    "url": "/react_news_app/static/js/2.7555418d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_news_app/static/js/2.7555418d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b1751b49ddd49430560",
+    "url": "/react_news_app/static/js/main.95acd17d.chunk.js"
+  },
+  {
+    "revision": "f3ee35d61c33ac0e54e9",
+    "url": "/react_news_app/static/js/runtime-main.014340fd.js"
+  }
+]);
