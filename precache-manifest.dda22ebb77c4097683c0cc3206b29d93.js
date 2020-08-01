df631ece0d3ae483e8b66a8af669d3e7f8e0c86c
boukamass <boukamass@gmail.com>
2020-08-01 10:11:50 -0500

    Updates

--- precache-manifest.dda22ebb77c4097683c0cc3206b29d93.js
+++ precache-manifest.dda22ebb77c4097683c0cc3206b29d93.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "65d7cf5b7d237c92df32daabe9d7e6bf",
+    "revision": "33e05ed8b914522b10d5de6cf97284b4",
     "url": "/react_news_app/index.html"
   },
   {
-    "revision": "2e8fa03a28de174b3577",
+    "revision": "20d52878ac50590963df",
     "url": "/react_news_app/static/css/main.65d59e5c.chunk.css"
   },
   {
-    "revision": "eb16acb31da01aeda56c",
-    "url": "/react_news_app/static/js/2.8015e9ef.chunk.js"
+    "revision": "e8b0caa134f5009c0f94",
+    "url": "/react_news_app/static/js/2.dabd07ed.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react_news_app/static/js/2.8015e9ef.chunk.js.LICENSE.txt"
+    "url": "/react_news_app/static/js/2.dabd07ed.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2e8fa03a28de174b3577",
-    "url": "/react_news_app/static/js/main.afe220f8.chunk.js"
+    "revision": "20d52878ac50590963df",
+    "url": "/react_news_app/static/js/main.6df574b9.chunk.js"
   },
   {
     "revision": "f3ee35d61c33ac0e54e9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_news_app/precache-manifest.c5539c9e3dc31805608d5596a0645509.js"
+  "/react_news_app/precache-manifest.dda22ebb77c4097683c0cc3206b29d93.js"
 );
 
 self.addEventListener('message', (event) => {
